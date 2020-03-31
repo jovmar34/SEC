267cb94d2bb68a20699550278660491620567329
rodrigojflima <rodrigo.j.f.lima@tecnico.ulisboa.pt>
2020-03-31 22:17:30 +0100

    Persistence done! Working nice!

--- README.md
+++ README.md
@@ -44,7 +44,6 @@ User | Password | Hash (SHA 256)
 client1 | 25HtkQDXEQRT | 9bd915291749076d56d4198b4ea35003249be5c88acebce51fcf559d52bde24e
 client2 | 5n2y5xcvkU86 | 4416f05dcc94e63edddd1e7459caefc6eb3137932ea64d446a08b2301aaefac6
 client3 | SqPxGv9bKpqp | 27d728e7c5ed0f593fce0b49518a9d470826cac65778c5b5d2e14e2302db7636
-server  | admin        | 8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918
 
 ## Password for Keystore
 
--- announcementServer/announcementServer-ws/src/main/java/org/announcementserver/utils/PersistenceUtils.java
+++ announcementServer/announcementServer-ws/src/main/java/org/announcementserver/utils/PersistenceUtils.java
@@ -10,7 +10,8 @@ import org.announcementserver.ws.AnnouncementServer;
 
 public class PersistenceUtils {
 	
-	public static final String FILENAME = "src/main/resources/serverstate.ser";
+	public static final String FILENAME = "src/main/resources/serverState.ser";
+	public static final String BACKUP_FILENAME = "src/main/resources/serverStateBackup.ser";
 	
 	public static void recover() {
 		FileInputStream fis = null;
@@ -24,20 +25,34 @@ public class PersistenceUtils {
 			ois.close();
 			fis.close();
 		} catch(Exception i) {
-			i.printStackTrace();
+			try {
+				fis = new FileInputStream(new File(BACKUP_FILENAME));
+				ois = new ObjectInputStream(fis);
+				AnnouncementServer announcementServer = (AnnouncementServer) ois.readObject();
+				AnnouncementServer.setInstance(announcementServer);
+			} catch(Exception j) {
+				j.printStackTrace();
+			}
 		}
 	}
 	
 	public static void serialize(AnnouncementServer announcementServer) {
 		FileOutputStream fos = null;
+		FileOutputStream fosb = null;
 		ObjectOutputStream oos = null;
+		ObjectOutputStream oosb = null;
 		
 		try {
 			fos = new FileOutputStream(new File(FILENAME));
+			fosb = new FileOutputStream(new File(BACKUP_FILENAME));
 			oos = new ObjectOutputStream(fos);
+			oosb = new ObjectOutputStream(fosb);
 			oos.writeObject(announcementServer);
+			oosb.writeObject(announcementServer);
 			oos.close();
+			oosb.close();
 			fos.close();
+			fosb.close();
 		} catch(Exception i) {
 			i.printStackTrace();
 		}
